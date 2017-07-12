e7a75093e2079767f841823189aff4be3cf099e4
Wee Lee <wenlilearn@gmail.com>
2017-07-12 00:25:55 -0400

    Better solution to add_one

--- plus_one/main.rb
+++ plus_one/main.rb
@@ -8,37 +8,66 @@ The digits are stored such that the most significant digit is at the head of the
 module Main
   # @param {Integer[]} digits
   # @return {Integer[]}
+  # def plus_one(digits)
+  #   if digits == []
+  #     return []
+  #   end
+  #
+  #   carry = 1
+  #   length = digits.length
+  #
+  #   digits = digits.reverse
+  #
+  #   digits.each_with_index do |val, i|
+  #       val = val + carry
+  #
+  #       if val >= 10
+  #         carry = 1
+  #         val = val % 10
+  #       else
+  #         carry = 0
+  #       end
+  #
+  #       digits[i] = val
+  #     end
+  #
+  #     digits = digits.reverse
+  #
+  #     if carry == 1
+  #       digits.insert(0, carry)
+  #       carry = 0
+  #     end
+  #     digits
+  #
+  #   end
+
   def plus_one(digits)
     if digits == []
       return []
     end
 
+    i = digits.length - 1
+
     carry = 1
-    length = digits.length
 
-    digits = digits.reverse
+    while(i >= 0) do
+      val = digits[i] + carry
 
-    digits.each_with_index do |val, i|
-        val = val + carry
-
-        if val >= 10
-          carry = 1
-          val = val % 10
-        else
-          carry = 0
-        end
-
-        digits[i] = val
-      end
-
-      digits = digits.reverse
-
-      if carry == 1
-        digits.insert(0, carry)
+      if val >= 10
+        val = val % 10
+        carry = 1
+      else
         carry = 0
       end
-      digits
-
+      digits[i] = val
+      i -= 1
     end
 
+    if carry == 1
+      digits.insert(0, 1)
+    end
+
+    carry = 0
+    digits
+  end
 end
